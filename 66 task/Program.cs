7c8f7653d4ec7851f1f024a1ae695d05ddf9c70a
Mansur Dagaev <mans.4e4en@mail.ru>
2023-02-06 21:34:18 +0300

    1

---
+++ 66 task/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+					
+public class Program
+{
+	public static void Main()
+	{
+
+
+
+int CalculateSumm(int m, int n)
+		{
+			int start = m;
+			int end = n;
+			if(m > n)
+			{
+				start = n;
+				end = m;
+			}
+			return (end + start)*(end - start + 1)/2;
+		}
+		
+		Console.WriteLine(CalculateSumm(72, 16));
+	}
+}
